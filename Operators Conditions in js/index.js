e2a456ba3d42cb329f869bca741497a4c89f579f
Dev Verma <devv6687@gmail.com>
2025-08-19 19:50:47 +0530

    Create index.js

---
+++ Operators Conditions in js/index.js
@@ -0,0 +1,53 @@
+console.log("Studying conditional statements ");
+let a=2;
+let b=20;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(b/a);
+let c = 3;
+console.log(b%c);
+console.log(b**a);
+console.log(b+=a);
+console.log(b!=a);
+let d = "3";
+console.log(c===d);
+// single line cmnt
+/*this is a 
+multi line 
+comment*/
+let age = 22;
+if (age>18){
+    console.log("driving valid");
+}
+else{
+    console.log("Driving invalid")
+}
+
+let age2 = 16;
+if (age2 > 18){
+console.log("Valid");
+}
+else if (age2 == 0){
+    console.log("Are you kidding?");
+}
+else if (age2+=2 == 18){
+console.log("valid after grace")
+}
+else{
+    console.log("Not valid")
+}
+
+let x = 6;
+let y = 9;
+let z = x>y ? (x-y):(y-x);
+console.log(z);
+
+
+
+
+
+
+
+
+
