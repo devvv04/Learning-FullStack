3ffa00be31dfac17789331c0936de11424c91646
Dev Verma <devv6687@gmail.com>
2025-08-19 19:03:01 +0530

    Add files via upload

---
+++ Variable & DataType in js/script.js
@@ -0,0 +1,53 @@
+alert("second js vid");
+var a = 2;
+var b = 7;
+var c = "dev";
+var d = true;
+console.log(a,b,c,d);
+console.log(typeof a,typeof b,typeof c,typeof d);
+let e = 45;
+let f = "also dev";
+const g = 3.14;
+
+
+// g = g + 3;
+// console.log(g);
+   
+
+
+{
+    let e = 875;
+    console.log(e);
+}
+ console.log(e);
+
+
+
+ console.log(a);
+ {
+    var a = 564;
+    console.log(a);
+ }
+ console.log(a);
+
+ let h = null;
+ let i = 23;
+ let j = "dev";
+ let k = undefined;
+ let l = true;
+ 
+ console.log(h,i,j,k,l);
+ console.log(typeof h,typeof i,typeof j,typeof k,typeof l);
+
+
+ let obj = {
+    name : "Dev",
+    Age : 21,
+    "Phone Number" : 9474264719,
+    is_handsome : true
+ }
+ console.log(obj);
+ obj.salary = 3452532;
+ console.log("Updated obj :" );
+  console.log(obj);
+console.log("value of c = "+c);
