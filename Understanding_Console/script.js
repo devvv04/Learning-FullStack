898285a26453a47cf60109d32d4b2ff841cf7ebc
Dev Verma <devv6687@gmail.com>
2025-08-19 18:19:55 +0530

    Add files via upload

---
+++ Understanding_Console/script.js
@@ -0,0 +1,13 @@
+    alert("Hello, World!");
+ console.log("This is a console log message.");
+ console.log("This is also running");
+ var a = prompt("Please enter your phone number");
+    console.log("Your phone number is: " + a);
+    
+    var isTrue = confirm("do you want to burst?")
+    if(isTrue){
+        console.log("bursted")
+    }
+    else{
+        console.log("not bursted")
+    }
