ba86d6b3beed9b39db2204e0ed709166ba7dae8e
Dev Verma <devv6687@gmail.com>
2025-08-21 18:12:00 +0530

    Create script.js

---
+++ functions in js/script.js
@@ -0,0 +1,22 @@
+console.log("code for functions");
+
+// function hello(name) {
+//     console.log("Hello " + name + ", How are you?");
+// }
+// hello("Dev");
+function sum(a,b,c){
+    // console.log(a+b);
+    console.log("the sum of " + a +", " +b+" and "+c+" is: " )
+    console.log(a+b+c);
+}
+// sum(3,5);
+// result = sum(2,3,4);
+sum(2,3,4);
+// console.log("the sum of these no is : "+ result);
+
+const func1 = (x) => {
+    console.log("i am an arrow function",x)
+}
+func1(342);
+func1(3321313);
+func1("dev");
