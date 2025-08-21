d6f1b1fda120652a16e1efa0c8443fa10b8fd5d6
Dev Verma <devv6687@gmail.com>
2025-08-21 19:53:11 +0530

    Create index.js

---
+++ Strings/index.js
@@ -0,0 +1,15 @@
+let a = "Verma";
+console.log(a);
+console.log(a[2]);
+b="dev";
+c=18;
+console.log(`Hellow ${b}, you are ${c} years old`);
+// console.log("bha kfhehf ehfw kefbiuewh flwehf");
+console.log(a.toLowerCase());
+console.log(a.toUpperCase());
+console.log(a.length);
+console.log(a.slice(2));
+console.log(a.slice(1.3));
+console.log(a.concat(" Hellow"," How"));
+console.log(a.replace("er","55"));
+
