ac799b3a4065340fb7b67cc44a256324e7334422
Dev Verma <devv6687@gmail.com>
2025-08-26 20:00:05 +0530

    Add files via upload

---
+++ Factorial/fact.js
@@ -0,0 +1,7 @@
+let a = prompt("Enter a number:");
+let i;
+let fact = 1;
+for (i=1;i<=a;i++){
+fact = fact * i
+}
+console.log(`Factorial of ${a} is : ${fact}`);
---
+++ Factorial/script.js
@@ -0,0 +1,5 @@
+//factorial using reduce and for loops
+let a = [5,4,3,2,1]
+console.log(a.reduce((a,b)=>{
+    return a*b
+}))
